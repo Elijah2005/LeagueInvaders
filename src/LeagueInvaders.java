1372af6f51a2d9857ef01ccfcf0a0e4b4c880ef7
TJHL <league@WTS15.attlocal.net>
2017-11-30 18:30:07 -0800

    not done

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+
+	JFrame display = new JFrame();
+public static void main(String[] args) {
+	LeagueInvaders league = new LeagueInvaders();
+}
+
+	LeagueInvaders(){
+	display.setSize(500, 800);
+	}	
+void setup() {
+	display.setVisible(true);
+	
+}
+}
