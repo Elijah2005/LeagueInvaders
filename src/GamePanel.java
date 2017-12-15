3188f5466cb00523b5c6854164bfd150c0448a8c
TJHL <league@WTS15.attlocal.net>
2017-12-14 18:23:33 -0800

    ON STEP 7

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,19 +1,37 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer clock;
+	final int MENU_STATE = 0;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	int CURRENT_STATE = MENU_STATE;
 	  @Override
 	  public void paintComponent(Graphics g){
-	                  
-		  g.fillRect(10, 10, 100, 100);     
+		  if(CURRENT_STATE == MENU_STATE){
+
+	             drawMenuState(g);
+
+	     }else if(CURRENT_STATE == GAME_STATE){
+
+	             drawGameState(g);
+
+	     }else if(CURRENT_STATE == END_STATE){
+
+	             drawEndState(g);
+
+	     }
+		  
 }
 	GamePanel() {
-
 		clock = new Timer(1000 / 60, this);
 	}
 
@@ -21,11 +39,74 @@ public class GamePanel extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		repaint();
+		 if(CURRENT_STATE == MENU_STATE){
+
+             updateMenuState();
+
+     }else if(CURRENT_STATE == GAME_STATE){
+
+             updateGameState();
+
+     }else if(CURRENT_STATE == END_STATE){
+
+             updateEndState();
+
+     }
+		 
 }
 
 	
 
 	void startGame() {
 		clock.start();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		CURRENT_STATE +=1;
+		if(CURRENT_STATE > END_STATE){
+
+            CURRENT_STATE = MENU_STATE;
+
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	void updateMenuState() {
+		
+	}
+	void updateGameState() {
+		
+	}
+	void updateEndState() {
+		
+	}
+	
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+
+		g.fillRect(0, 0, LeagueInvaders.width, LeagueInvaders.height);    
+
+
+	}
+	void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+
+		g.fillRect(0, 0, LeagueInvaders.width, LeagueInvaders.height);    
+
+	}
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+
+		g.fillRect(0, 0, LeagueInvaders.width, LeagueInvaders.height);    
+
 	}
 }
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,12 +1,14 @@
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
-public class LeagueInvaders {
+public class LeagueInvaders{
 	GamePanel game;
 	JFrame display = new JFrame();
-	int width = 500;
-	int height = 800;
+	static final int width = 500;
+	static final int height = 800;
 
 	public static void main(String[] args) {
 		LeagueInvaders league = new LeagueInvaders();
@@ -17,9 +19,10 @@ public class LeagueInvaders {
 		display.setSize(width, height);
 		display.setVisible(true);
 		display.setDefaultCloseOperation(display.EXIT_ON_CLOSE);
-		game = new GamePanel();
+		 game = new GamePanel();
 		display.getContentPane().setPreferredSize(new Dimension(width, height));
 		display.pack();
+		display.addKeyListener(game);
 		setup();
 	}
 
@@ -27,4 +30,6 @@ public class LeagueInvaders {
 		display.add(game);
 		game.startGame();
 	}
+
+
 }
